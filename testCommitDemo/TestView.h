38926a5be71004d56fe8d6d99430814e39d18285
cquxutao <178052394@qq.com>
2017-11-24 19:27:18 +0800

    add x

---
+++ testCommitDemo/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  testCommitDemo
+//
+//  Created by tennyxu on 24/11/2017.
+//  Copyright © 2017 tennyxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ testCommitDemo/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  testCommitDemo
+//
+//  Created by tennyxu on 24/11/2017.
+//  Copyright © 2017 tennyxu. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
